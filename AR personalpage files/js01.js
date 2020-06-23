4f76af452761794a4faefc42e4981105498c832c
Alphin Roy <alphinroy430@gmail.com>
2020-06-24 01:46:08 +0530

    nav but cheriya prahsnam und pad

--- AR personalpage files/js01.js
+++ AR personalpage files/js01.js
@@ -3,16 +3,29 @@
     'use strict';
     $('.hamburger-menu').click(function (e) {
         e.preventDefault();
-        if ($(this).hasClass('active')){
-            $(this).removeClass('active');
+        
+            $(this).toggleClass('active');
             $('.menu-overlay').fadeToggle( 'fast', 'linear' );
-            $('.menu .menu-list').slideToggle( 'slow', 'swing' );
+            $('.menu .menu-list ').slideToggle( 'slow', 'swing' );
             $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
-        } else {
-            $(this).addClass('active');
-            $('.menu-overlay').fadeToggle( 'fast', 'linear' );
-            $('.menu .menu-list').slideToggle( 'slow', 'swing' );
-            $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
-        }
+        
     })
 })();
+  
+// (function() {
+//     'use strict';
+//     $('.hamburger-menu').click(function (e) {
+//         e.preventDefault();
+//         if ($(this).hasClass('active')){
+//             $(this).removeClass('active');
+//             $('.menu-overlay').fadeToggle( 'fast', 'linear' );
+//             $('.menu .menu-list ').slideToggle( 'slow', 'swing' );
+//             $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+//         } else {
+//             $(this).addClass('active');
+//             $('.menu-overlay').fadeToggle( 'fast', 'linear' );
+//             $('.menu .menu-list ').slideToggle( 'slow', 'swing' );
+//             $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+//         }
+//     })
+// })();
